947d096ac2912209030c587fb1318481140cdff5
VENOM <117318674+VENOM7XxX@users.noreply.github.com>
2023-02-12 07:12:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph//file/835b48f330763bffc34a7.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph//file/835b48f330763bffc34a7.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/08d12aa31c5c41e51f6f2.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/08d12aa31c5c41e51f6f2.jpg")
 
 SESSION = getenv("SESSION", None)
 
