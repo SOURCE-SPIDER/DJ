07ac05ecd6648b9b9a923202209078c791a9fabf
VENOM <117318674+VENOM7XxX@users.noreply.github.com>
2023-02-17 13:36:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/08d12aa31c5c41e51f6f2.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/08d12aa31c5c41e51f6f2.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/e1ca90fe34e9640e0248b.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/e1ca90fe34e9640e0248b.jpg")
 
 SESSION = getenv("SESSION", None)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/EE_47")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MRv7x")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EE_20")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/e1ca90fe34e9640e0248b.jpg"
